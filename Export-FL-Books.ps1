305218061462ca0aff475ad246be0851da60055a
Azuregos <pospishniy@gmail.com>
2017-08-20 18:26:08 +0200

    API export from fantlab. Script documenttation.

--- Export-FL-Books.ps1
+++ Export-FL-Books.ps1
@@ -1,7 +1,81 @@
+<#
+
+.SYNOPSIS
+    Export rated books from the fantlab.ru site into a number of supported formats 
+
+.DESCRIPTION
+    This script will perform a series of HTTP requests to a fantlab.ru and api.fantlab.ru websites 
+    to collect a list of books rates by a user, specified in script parameters.
+    No authentication is needed to collect data.
+
+.PARAMETER Username
+    A username for whom books will be exported. 
+    This is not a login name, but a user name seen in the URL on your profile page: http://fantlab.ru/userXXXX 
+    'userXXXX' is an expected Username format for this script.
+
+.PARAMETER OutputFormat
+    The representation of the exported information. One of the: 'Table','JSON','XML','CSV' or 'Null'
+
+.PARAMETER OutFile
+    Write collected information to a file, specefied by this parameter.
+
+.PARAMETER UseJsonAPI
+    Use api.fantlab.ru API to retrieve additional indormation about books. 
+    If set to false, only basic information will be exported: Author(RUS), Title(RUS and ENG), Score
+
+.EXAMPLE
+    Output all books for user User1234 to console in Table format
+    .\Export-FL-Books.ps1 -Username User1234 
+
+.EXAMPLE
+    Output all books for user User1234 to console in JSON format
+    .\Export-FL-Books.ps1 -Username User1234 -OutputFormat JSON
+
+.EXAMPLE
+    Write all books for user User1234 to export.xlm in XML format
+    .\Export-FL-Books.ps1 -Username User1234 -OutputFormat XML -OutFile ".\export.xlm" 
+
+.NOTES
+    Author: Pospishnyi Oleksandr
+    Date:   Aug 20, 2017   
+
+.LINK
+    https://github.com/azuregos/FantReads
+#>
+
 param (
-    [string]$Username
+    [Parameter(Mandatory=$true, Position=0, 
+    HelpMessage="Fantlab.ru user to import data from (this is not a login name, but a real user name seen in the URL on your profile page")]
+    [ValidateNotNullOrEmpty()]
+    [string]$Username,
+
+    [Parameter(Mandatory=$false)]  
+    [ValidateSet('Table','JSON','XML','CSV', 'Null')]
+    [string]$OutputFormat = 'Table',
+
+    [Parameter(Mandatory=$false)] 
+    [string]$OutFile,
+
+    [Parameter(Mandatory=$false)] 
+    [boolean]$UseJsonAPI = $true
 )
 
 Import-Module .\ps-modules\Fantlab-Utils.ps1 -Force
 
-Get-Books -URI 'https://fantlab.ru/$($Username)/marks'
+$books = Get-Books -URI "https://fantlab.ru/$($Username)/marks" -UseJsonAPI $UseJsonAPI
+
+switch ($OutputFormat) {
+    'Table' { $out = $books | Format-Table }
+    'JSON'  { $out = $books | ConvertTo-Json }
+    'XML'   { $out = $books | ConvertTo-Xml -As String }
+    'CSV'   { $out = $books | ConvertTo-Csv}
+    Default { $out = ""}
+}
+
+if ($OutFile) {
+    Write-Verbose "Write to: $($OutFile)"
+    $out | Out-File $OutFile
+} else {
+    Write-Verbose "Result:"
+    Write-Output $out 
+}
--- ps-modules/Fantlab-Utils.ps1
+++ ps-modules/Fantlab-Utils.ps1
@@ -3,13 +3,14 @@ function Get-Books
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true)]  [string]$URI,
-        [Parameter(Mandatory=$false)] [boolean]$FollowPages = $true
+        [Parameter(Mandatory=$false)] [boolean]$FollowPages = $true,
+        [Parameter(Mandatory=$false)] [boolean]$UseJsonAPI = $true
     )
 
     Write-Host "Reading books from $($URI): " -ForegroundColor Green -NoNewline
 
     # Get starting Fantlab ratings page (unparsed)
-    $pageRaw = Invoke-WebRequest -Uri $URI -TimeoutSec 10 -DisableKeepAlive  -Method Get -UseBasicParsing 
+    $pageRaw = Invoke-WebRequest -Uri $URI -TimeoutSec 10 -DisableKeepAlive  -Method Get -UseBasicParsing
 
     #Find Table with relevant data
     $tableStart = $pageRaw.RawContent.IndexOf("<table");
@@ -44,17 +45,23 @@ function Get-Books
                 $book = $null
                 break;
             } else {
-                $book = ParseBook $record.innerText
+                if ($UseJsonAPI) {
+                    $book = ReadBook $record.innerHTML
+                } else {
+                    $book = ParseBook $record.innerText
+                }
             }
         }
     }
 
     Write-Host "$($books.Count) Books Found" -ForegroundColor White
 
+    #Repeat for all other pages (books are displayed in blocks of 200)
     if ($FollowPages)
     {
         $pages = $pageRaw.Links | Where-Object {$_.href -And $_.href.IndexOf('markspage') -gt 0} | Select-Object -Property href -Unique
         
+        Write-Verbose "Other Pages to process: $($pages.Count)"
         foreach ($page in $pages) 
         {
             $pageURI = "https://fantlab.ru" + $page.href;
@@ -70,6 +77,7 @@ function ParseBook
 {
     param ([string]$text)
 
+    Write-Debug "Parsing book: $($text)"
     $book = New-Object PSCustomObject
     $startQuote = [char]0x00ab  # «
     $endQuote = [char]0x00bb    # »
@@ -87,9 +95,9 @@ function ParseBook
             $strIdx = $parts.IndexOf($startQuote) + 1
             $endIdx = $parts.IndexOf($endQuote) 
             $title =  $parts.Substring($strIdx, $endIdx - $strIdx).Trim()
-            $book | Add-Member -type NoteProperty -name "Author" -Value $author
+            $book | Add-Member -type NoteProperty -name "AuthorRU" -Value $author
             $book | Add-Member -type NoteProperty -name "TitleRU" -Value $title
-            $book | Add-Member -type NoteProperty -name "TitleENG" -Value "-"
+            $book | Add-Member -type NoteProperty -name "TitleEN" -Value "-"
          }
         2 {
             $strIdx = $parts[0].IndexOf('.') + 1
@@ -97,14 +105,39 @@ function ParseBook
             $author = $parts[0].Substring($strIdx, $endIdx - $strIdx).Trim()
             $strIdx = $parts[0].IndexOf($startQuote) + 1
             $endIdx = $parts[0].IndexOf($endQuote) 
-            $titleRu =  $parts[0].Substring($strIdx, $endIdx - $strIdx).Trim()
+            $titleRu = $parts[0].Substring($strIdx, $endIdx - $strIdx).Trim()
             $strIdx = $parts[1].IndexOf($startQuote) + 1
             $endIdx = $parts[1].IndexOf($endQuote) 
-            $titleEn =  $parts[1].Substring($strIdx, $endIdx - $strIdx).Trim()
-            $book | Add-Member -type NoteProperty -name "Author" -Value $author
+            $titleEn = $parts[1].Substring($strIdx, $endIdx - $strIdx).Trim()
+            $book | Add-Member -type NoteProperty -name "AuthorRU" -Value $author
             $book | Add-Member -type NoteProperty -name "TitleRU" -Value $titleRu
-            $book | Add-Member -type NoteProperty -name "TitleENG" -Value $titleEn
+            $book | Add-Member -type NoteProperty -name "TitleEN" -Value $titleEn
          }
+    }
+    return $book
+}
+
+function ReadBook
+{
+    param ([string]$html)
+    
+    $book = New-Object PSCustomObject
+    $strIdx = $html.IndexOf("/work") + 5
+    If ($strIdx-5 -gt 0)
+    {
+        $endIdx = $html.IndexOf('"', $strIdx)
+        $workId = $html.Substring($strIdx, $endIdx - $strIdx)
+        Write-Verbose "Get information about work$($workId)"
+        
+        #Invoke JSON API: https://api.fantlab.ru/work/XXXX
+        $apiURI = "https://api.fantlab.ru/work/$($workId)"
+        $json = Invoke-RestMethod -Uri $apiURI -Method Get -TimeoutSec 10
+
+        $book | Add-Member -type NoteProperty -name "AuthorRU" -Value $json.authors[0].name
+        $book | Add-Member -type NoteProperty -name "TitleRU" -Value $json.work_name
+        $book | Add-Member -type NoteProperty -name "AuthorEN" -Value $json.authors[0].name_orig
+        $book | Add-Member -type NoteProperty -name "TitleEN" -Value $json.work_name_orig
+        $book | Add-Member -type NoteProperty -name "Type" -Value $json.work_type_name
     }
     return $book
 }
